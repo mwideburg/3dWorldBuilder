32911054097c2fa0ce3e01205cc9eec8bb654040
Michael Wideburg <mwideburg@gmail.com>
2023-07-23 15:52:21 -0400

    Auto-generated commit [ci skip]

--- .prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    trailingComma: "all",
-    tabWidth: 4,
-    semi: true,
-    singleQuote: false,
-    bracketSpacing: true,
-    printWidth: 100,
-};
--- src/app/components/types/dimensionType.ts
+++
@@ -1,5 +0,0 @@
-export type Dimension = {
-    width: number;
-    height: number;
-    depth: number;
-};
