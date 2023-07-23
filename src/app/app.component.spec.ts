74589360bcff45168b19a0c21ac199f33b218990
Michael Wideburg <mwideburg@gmail.com>
2023-07-23 16:35:00 -0400

    clean up

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ms-template'`, () => {
+  it(`should have as title '3d-world-builder'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ms-template');
+    expect(app.title).toEqual('3d-world-builder');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ms-template app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('3d-world-builder app is running!');
   });
 });
